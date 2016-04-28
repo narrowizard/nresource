a51999d2e6736b1288d58b2a7c9b1ccb0c6be258
Narro <wangc@jiudeng.me>
2016-04-28 17:13:24 +0800

    更新readme文件

--- README.md
+++ README.md
@@ -5,10 +5,21 @@
 + /js/{filename}
 + /css/{filename}
 + /sass/{filename}
++ /static/{filename}
 
-{filename}:可以包含目录,目录间用-连接
+{filename}:可以包含目录,目录间用|连接
 
 ## config
 配置文件:webconfig.json
 配置项
 + port  监听的http端口
++ contentPath   静态文件目录
++ cachePath 缓存文件目录
++ mode 运行模式,debug或release
++ maxAge 浏览器缓存时间
+        maxAge:{
+            days:number
+            hours: number,
+            minutes: number,
+            seconds: number
+        }
--- router.js
+++ router.js
@@ -2,7 +2,7 @@ var crossroads = require('crossroads');
 var js_controller = require('./controller/js_controller').handler;
 var css_controller = require('./controller/css_controller').handler;
 var sass_controller = require('./controller/sass_controller').handler;
-
+var static_controller = require('./controller/static_controller').handler;
 crossroads.ignoreState = true;
 
 //js处理器
@@ -11,6 +11,7 @@ crossroads.addRoute('/js/{filename}', js_controller);
 crossroads.addRoute('/css/{filename}', css_controller);
 //sass处理器
 crossroads.addRoute('/sass/{filename}', sass_controller);
-
+//静态文件处理器
+crossroads.addRoute('/static/{filename}', static_controller);
 
 exports.router = crossroads;
