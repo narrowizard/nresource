57424aead0ff4caebf2688d6a149d90a67978aa4
Narro <wangc@jiudeng.me>
2016-05-03 13:24:34 +0800

    解决了关闭缓存后,静态路由出现的问题

--- controller/static_controller.js
+++ controller/static_controller.js
@@ -1,4 +1,48 @@
+var fs = require('fs');
+var mime = require('mime');
+var log = require('../utils/log');
+var zlib = require('zlib');
+
 exports.handler = function (req, res, compress, filename) {
-    res.writeHead(404, "file not found!");
-    res.end();
+    if (!global.USECACHE) {
+        var filepath = global.CACHEPATH + "static/" + filename;
+        fs.stat(filepath, function (err, stats) {
+            if (err) {
+                log.error(err);
+            } else if (stats.isFile()) {
+                var requestHeaders = req.headers;
+                res.setHeader("Content-Type", mime.lookup(filename) + ";charset=utf-8");
+                var lastModified = stats.mtime.toUTCString();
+                //如果没有修改，则返回304
+                if (requestHeaders["if-modified-since"] && lastModified == requestHeaders["if-modified-since"]) {
+                    res.writeHead(304, "Not Modified");
+                    res.end();
+                    return;
+                }
+                res.setHeader("Last-Modified", lastModified);
+                //将文件写入response
+                res.statusCode = 200;
+                var file = fs.createReadStream(filepath);
+                file.on("open", function () {
+                    if (compress === "gzip") {
+                        file.pipe(zlib.createGzip()).pipe(res);
+                    } else if (compress === "deflate") {
+                        file.pipe(zlib.createDeflate()).pipe(res);
+                    } else {
+                        file.pipe(res);
+                    }
+
+                });
+                file.on('error', function (err) {
+                    log.error(err);
+                });
+                return;
+            }
+            res.writeHead(404, "file not found!");
+            res.end();
+        });
+    } else {
+        res.writeHead(404, "file not found!");
+        res.end();
+    }
 }
