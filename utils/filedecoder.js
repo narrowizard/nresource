125f81298b3ae5f2314a5ad0a709df1d254fbdbc
Narro <wangc@jiudeng.me>
2016-05-06 15:57:56 +0800

    bug fixed

--- utils/filedecoder.js
+++ utils/filedecoder.js
@@ -1,10 +1,11 @@
+var log = require('./log');
 
 exports.decodeFileName = function (filename, extName) {
     //解码filename
     decodeName = decodeURIComponent(filename);
     var files = decodeName.split("&");
     for (var i = 0; i < files.length; i++) {
-        if (i = files.length - 1) {
+        if (i == files.length - 1) {
             //最后一个自带了扩展名
             files[i] = files[i].substr(0, files[i].lastIndexOf("."));
         }
