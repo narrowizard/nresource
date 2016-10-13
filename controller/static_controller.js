0b313707697ed48b3309cd690e17a1a4f96050f7
narro <501xs05@163.com>
2016-10-13 15:35:09 +0800

    修复static路由不支持中文文件名的bug

--- controller/static_controller.js
+++ controller/static_controller.js
@@ -5,21 +5,20 @@ var zlib = require('zlib');
 var responseFile = require('../utils/file').responseFile;
 
 exports.handler = function (req, res, compress, filename) {
-    if (!global.USECACHE) {
-        var filepath = global.CACHEPATH + "static/" + filename;
-        fs.stat(filepath, function (err, stats) {
-            if (err) {
-                log.error(err);
-            } else if (stats.isFile()) {
-                responseFile(req, res, filepath, stats, compress, filename);
-            }
-            log.info("[NotFound]");
-            res.writeHead(404, "file not found!");
-            res.end();
-        });
-    } else {
+    // 不使用缓存时,静态路由需要单独处理
+    // 文件名支持unicode
+    filename = decodeURIComponent(filename);
+    var filepath = global.CACHEPATH + "static/" + filename;
+    fs.stat(filepath, function (err, stats) {
+        if (err) {
+            log.error(err);
+        } else if (stats.isFile()) {
+            responseFile(req, res, filepath, stats, compress, filename);
+            return;
+        }
         log.info("[NotFound]");
         res.writeHead(404, "file not found!");
         res.end();
-    }
+    });
+
 }
